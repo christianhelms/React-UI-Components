5f4434989a0f99462a7704c0a1123aad94aedf3e
christianhelms <fonticons-dev@Fonticonss-MBP.hitronhub.home>
2019-05-28 18:41:22 -0500

    project

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Button.css';
+
+const ActionButton = props => {
+    return <button className={props.buttonStyle}>{props.textProp.str}</button>;
+};
+
+    export default ActionButton;
+
+const props = {
+  textProp: { str: 'Hello CodeSandbox' }
+};
