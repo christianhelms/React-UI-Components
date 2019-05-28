bfad0738f2815f3ebbcfc55156a437a084613ea7
christianhelms <fonticons-dev@Fonticonss-MBP.hitronhub.home>
2019-05-28 16:12:59 -0500

    display finished

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Display.css';
+
+function CalculatorDisplay() {
+    return (
+        <div class="displayContainer">
+            <h1>0</h1>
+        </div>
+    );
+}
+
+export default CalculatorDisplay;
+
